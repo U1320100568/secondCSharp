551fc737803fe56117973f4983a8f34ace513d3f
U1320100568 <35591116+U1320100568@users.noreply.github.com>
2018-02-22 00:15:21 +0800

    Add files via upload

---
+++ Class1.Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    partial class Class1
+    {
+        public string GetDate() {
+            return DateTime.Now.ToString("yyyy-MM--dd");
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public partial class Class1 : Interface1
+    {
+        public string GetNow() {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");   
+        }
+    }
+}
